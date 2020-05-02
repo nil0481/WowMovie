2f7c7cadcd57c3790988ba55b04311051c452689
Nilesh Singhania <nileshsinghania0481@gmail.com>
2020-05-03 03:31:27 +0530

    Initial commit

---
+++ app/src/main/java/com/nilesh/wowmovie/api/Service.java
@@ -0,0 +1,20 @@
+package com.nilesh.wowmovie.api;
+
+import com.nilesh.wowmovie.model.MoviesResponse;
+
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+
+public interface Service {
+
+    @GET("movie/popular")
+    Call<MoviesResponse> getPopularMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+
+}
